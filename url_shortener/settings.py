ae216dfc7448924442e72cda3743e2b56391a332
Mike Firer <firerml@gmail.com>
2016-08-01 02:05:38 -0400

    again

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -130,7 +130,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'url_shortener/static'),
-)
